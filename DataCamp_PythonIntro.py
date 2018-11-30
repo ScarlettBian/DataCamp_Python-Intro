6a3b14af2b0024196aa29bfbd57ece44826f59e1
Scarlett Bian <hellobyh@gmail.com>
2018-11-30 17:32:13 -0500

    Init commit

---
+++ DataCamp_PythonIntro.py
@@ -0,0 +1,829 @@
+########################### Section1 Python Basics ##########################
+
+
+# Example, do not modify!
+print(5 / 8)
+
+# Put code below here
+print (7+10)
+
+7+10
+
+# Division
+print(5 / 8)
+
+# Addition
+print(7 + 10)
+
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100*1.1**7)
+
+# Create a variable savings
+savings=100
+
+# Print out savings
+print(savings)
+
+
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor=1.10
+
+# Calculate result
+result=savings*factor**7
+
+# Print out result
+print(result)
+
+
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of factor and savings to year1
+year1=savings*factor
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc=desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float=float(pi_string)
+
+
+
+
+
+
+########################### Section2 Python Lists ##########################
+
+
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas=[hall, kit, liv,bed, bath]
+
+# Print areas
+print(areas)
+
+
+
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ["hallway", hall,"kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# Print out house
+print(house)
+# Print out the type of house
+print(type(house))
+
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area=areas[3]+areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs=areas[:6]
+
+# Use slicing to create upstairs
+upstairs=areas[-4:]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
+
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Alternative slicing to create downstairs
+downstairs=areas[:6]
+
+# Alternative slicing to create upstairs
+upstairs=areas[6:]
+
+
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+areas[-1]=10.5
+
+# Change "living room" to "chill zone"
+areas[4]="chill zone"
+
+
+
+
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1=areas+["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2=areas_1+["garage", 15.45]
+
+
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_copy = areas[:]
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
+
+
+
+########################### Section3 Functions and Packages ##########################
+
+
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2=int(var2)
+
+
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full=first+second
+
+# Sort full in descending order: full_sorted
+full_sorted=sorted(full, reverse=True)
+
+# Print out full_sorted
+print(full_sorted)
+
+
+
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up=place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count("o"))
+
+
+
+
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
+
+
+
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+areas.append(15.45)
+
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
+
+
+
+
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2*math.pi*r
+
+# Calculate A
+A = math.pi*r**2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+
+
+
+
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+from math import radians
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist=r * radians(12)
+
+# Print out dist
+print(dist)
+
+
+
+########################### Section4 NumPy ######################################
+
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball=np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+
+
+
+
+
+
+
+
+# height is available as a regular list
+#height=list（[74, 72, 72, 73, 69, 69, 71, 76, 71]）
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height: np_height
+np_height=np.array(height)
+
+# Print out np_height
+print(np_height)
+
+# Convert np_height to m: np_height_m
+np_height_m=0.0254*np_height
+
+# Print np_height_m
+print(np_height_m)
+
+
+
+
+# height and weight are available as regular lists
+
+# Import numpy
+import numpy as np
+
+# Create array from height with correct units: np_height_m
+np_height_m = np.array(height) * 0.0254
+
+# Create array from weight with correct units: np_weight_kg
+np_weight_kg=np.array(weight) *  0.453592
+
+# Calculate the BMI: bmi
+bmi=np_weight_kg/np_height_m**2
+
+# Print out bmi
+print(bmi)
+
+
+
+
+
+# height and weight are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+# Calculate the BMI: bmi
+np_height_m = np.array(height) * 0.0254
+np_weight_kg = np.array(weight) * 0.453592
+bmi = np_weight_kg / np_height_m ** 2
+
+# Create the light array
+light=bmi < 21
+
+# Print out light
+print(light)
+
+# Print out BMIs of all baseball players whose BMI is below 21
+print(bmi[light])
+
+
+
+
+
+# height and weight are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+# Store weight and height lists as numpy arrays
+np_weight = np.array(weight)
+np_height = np.array(height)
+
+# Print out the weight at index 50
+print(np_weight[50])
+
+# Print out sub-array of np_height: index 100 up to and including index 110
+print(np_height[100:111])
+
+
+
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Import numpy
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball=np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+
+
+
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball=np.array(baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+
+
+
+
+
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49,])
+
+# Select the entire second column of np_baseball: np_weight
+np_weight=np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123,0])
+
+
+
+
+# baseball is available as a regular list of lists
+# updated is available as 2D numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball+updated)
+
+# Create numpy array: conversion
+conversion=np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball*conversion)
+
+
+
+
+
+
+
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Create np_height from np_baseball
+np_height= np_baseball[:,0]
+
+# Print out the mean of np_height
+print(np.mean(np_height))
+
+# Print out the median of np_height
+print(np.median(np_height))
+
+
+
+
+
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
+print("Correlation: " + str(corr))
+
+
+
+
+
+# heights and positions are available as lists
+
+# Import numpy
+import numpy as np
+
+# Convert positions and heights to numpy arrays: np_positions, np_heights
+np_positions=np.array(positions)
+np_heights=np.array(heights)
+
+# Heights of the goalkeepers: gk_heights
+gk_heights=np_heights[np_positions=='GK']
+
+# Heights of the other players: other_heights
+other_heights=np_heights[np_positions!='GK']
+
+# Print out the median height of goalkeepers. Replace 'None'
+print("Median height of goalkeepers: " + str(np.median(gk_heights)))
+
+# Print out the median height of other players. Replace 'None'
+print("Median height of other players: " + str(np.median(other_heights)))
+
+
+
+
+
+
+
+########################### Section5 Matplotlib ##########################
+
+# Print the last item from year and pop
+print(year)
+print(pop)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+# Display the plot with plt.show()
+plt.show()
+
+
+
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap[-1])
+print(life_exp[-1])
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot(gdp_cap, life_exp)
+
+# Display the plot
+plt.show()
+
+
+
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale("log")
+
+# Show plot
+plt.show()
+
+
+
+
+# Import package
+import matplotlib.pyplot as plt
+
+# Build Scatter plot
+plt.scatter(pop, life_exp)
+        
+# Show plot
+plt.show()
+
+
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
+
+
+
+# Build histogram with 5 bins
+plt.hist(life_exp, bins=5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp, bins=20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+
+
+
+# Histogram of life_exp, 15 bins
+plt.hist(life_exp, bins=15)
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Histogram of life_exp1950, 15 bins
+plt.hist(life_exp1950, bins=15)
+
+# Show and clear plot again
+plt.show()
+plt.clf()
+
+
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
+
+
+
+# Scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks(tick_val, tick_lab)
+
+# After customizing, display the plot
+plt.show()
+
+
+
+
+
+# Import numpy as np
+import numpy as np
+
+# Store pop as a numpy array: np_pop
+np_pop=np.array(pop)
+
+# Double np_pop
+np_pop=np_pop*2
+
+# Update: set s argument to np_pop
+plt.scatter(gdp_cap, life_exp, s = np_pop)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
+
+# Display the plot
+plt.show()
+
+
+# Specify c and alpha inside plt.scatter()
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c=col, alpha=0.8)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Show the plot
+plt.show()
+
+
+
+# Scatter plot
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Additional customizations
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
+
+# Add grid() call
+plt.grid(True)
+
+# Show the plot
+plt.show()
+
+
+
+
+
+
+
+########################### Section 6 Dictionaries & Pandas ##########################
+
+
+########################### Section 7 Logic, Control Flow and Filtering ##########################
+
+
+########################### Section 8 Loops ##########################
+
+
+########################### Section 9 Case Study: Hacker Statistics ##########################
+
+
+remain=nums
+for i in range(len(remain)):
+    if nums[len(remain)-i-1] == val:
+        del nums[len(remain)-i-1]
